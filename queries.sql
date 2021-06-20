c87181175444830a005bcac75b6d3bf87183d2a3
sdunderhill <81715217+sdunderhill@users.noreply.github.com>
2021-06-20 15:25:48 -0500

    updating queries with challenge code

--- queries.sql
+++ queries.sql
@@ -36,3 +36,165 @@ WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
 
 select * from retirement_info;
+
+DROP TABLE retirement_info;
+
+-- create new table for retiring employees
+SELECT emp_no, first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+-- check the table
+SELECT * FROM retirement_info;
+
+-- Joining departments and dept_manager tables
+SELECT d.dept_name,
+     dm.emp_no,
+     dm.from_date,
+     dm.to_date
+FROM departments as d
+INNER JOIN dept_manager as dm
+ON d.dept_no = dm.dept_no;
+
+-- joining retirement_info and dept_emp tables
+SELECT ri.emp_no,
+	ri.first_name,
+	ri.last_name,
+	de.to_date
+	FROM retirement_info as ri
+	LEFT JOIN dept_emp as de
+	ON ri.emp_no = de.emp_no;
+	
+SELECT ri.emp_no, 
+	ri.first_name, 
+	ri.last_name, 
+	de.to_date
+INTO current_emp
+FROM retirement_info as ri
+LEFT JOIN dept_emp as de
+ON ri.emp_no = de.emp_no
+WHERE de.to_date = ('9999-01-01');
+
+-- employee count by department number
+SELECT COUNT(ce.emp_no), de.dept_no
+INTO retiring_dept
+FROM current_emp as ce
+LEFT JOIN dept_emp as de
+ON ce.emp_no = de.emp_no
+GROUP BY de.dept_no
+ORDER BY de.dept_no;
+
+SELECT * FROM salaries
+ORDER BY to_date DESC;
+
+SELECT emp_no, first_name, last_name, gender
+INTO emp_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+SELECT * FROM emp_info;
+drop table emp_info;
+
+-- create emp_info table
+SELECT e.emp_no,
+    e.first_name,
+	e.last_name,
+    e.gender,
+    s.salary,
+    de.to_date
+INTO emp_info
+FROM employees as e
+	INNER JOIN salaries as s
+		ON (e.emp_no = s.emp_no)
+	INNER JOIN dept_emp as de
+		ON (e.emp_no = de.emp_no)
+	WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+		AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31')
+		AND (de.to_date = '9999-01-01');
+	
+-- List of managers per department
+SELECT  dm.dept_no,
+        d.dept_name,
+        dm.emp_no,
+        ce.last_name,
+        ce.first_name,
+        dm.from_date,
+        dm.to_date
+INTO manager_info
+FROM dept_manager AS dm
+    INNER JOIN departments AS d
+        ON (dm.dept_no = d.dept_no)
+    INNER JOIN current_emp AS ce
+        ON (dm.emp_no = ce.emp_no);
+
+-- list of dept_info
+SELECT ce.emp_no,
+	ce.first_name,
+	ce.last_name,
+	d.dept_name
+INTO dept_info
+FROM current_emp as ce
+	INNER JOIN dept_emp as de
+		ON (ce.emp_no = de.emp_no)
+	INNER JOIN departments as d
+		ON (de.dept_no = d.dept_no);
+		
+-- list for Sales department
+SELECT ce.emp_no,
+	ce.first_name,
+	ce.last_name,
+	d.dept_name
+FROM current_emp as ce
+	INNER JOIN dept_emp as de
+		ON (ce.emp_no = de.emp_no)
+	INNER JOIN departments as d
+		ON (de.dept_no = d.dept_no)
+	WHERE (de.dept_no = 'd007');
+			
+-- list for Sales and development departments
+SELECT ce.emp_no,
+	ce.first_name,
+	ce.last_name,
+	d.dept_name
+FROM current_emp as ce
+	INNER JOIN dept_emp as de
+		ON (ce.emp_no = de.emp_no)
+	INNER JOIN departments as d
+		ON (de.dept_no = d.dept_no)
+	WHERE de.dept_no IN ('d007', 'd005');
+
+--retrieve info for retiring by title
+SELECT ce.emp_no,
+	ce.first_name,
+	ce.last_name,
+	ti.title,
+	ti.from_date,
+	ti.to_date
+--INTO retirement_titles
+FROM current_emp as ce
+	INNER JOIN titles as ti
+		ON (ce.emp_no = ti.emp_no)
+	INNER JOIN employees as e
+		ON (ce.emp_no = e.emp_no)
+	WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+	ORDER BY ce.emp_no;
+
+-- Use Dictinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON (emp_no) emp_no,
+	first_name,
+	last_name,
+	title
+
+INTO unique_titles
+FROM retirement_titles 
+ORDER BY emp_no, to_date DESC;
+
+
+--retiring numbers by title
+SELECT COUNT(title), 
+	title
+INTO retiring_titles
+FROM unique_titles
+GROUP BY title
+ORDER BY count DESC;
